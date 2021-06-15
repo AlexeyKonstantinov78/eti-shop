27dbe84de451f61ab4a53ec021a71e18dedb20d2
Aleksey Konstantinov <alex_kav@mail.ru>
2021-06-15 20:46:44 +0300

    скрипт рендера законцен

---
+++ db/db.js
@@ -0,0 +1,60 @@
+const dataDb = {
+    "kabel-provod-shina-shinoprovod": {
+        "name": "Кабель, провод, шина, шинопровод",
+        "items": {
+            "kabel-provod": {
+                "name": "Кабель и провод",
+                "img": "/image/kabel-provod.png",
+                "items": [
+                    {
+                        "name": "Кабель и провод силовой для стационарной установки (до 1 кВ)",
+                        "img": "/image/kab1.jpg",
+                        "URL": "https://etipro.ru/catalogs/EG000001/EC003248"
+                    },
+                    {
+                        "name": "Кабель и провод силовой для нестационарной установки (до 1 кВ)",
+                        "img": "/image/kab2.jpg",
+                        "URL": "https://etipro.ru/catalogs/EG000001/EC003250"
+                    },
+                    {
+                        "name": "Кабель/провод силовой для воздушных линий электропередач (ЛЭП)",
+                        "img": "/image/kab3.jpg",
+                        "URL": "https://etipro.ru/catalogs/EG000001/EC001139"
+                    },
+                    {
+                        "name": "Кабель силовой для стационарной установки (больше 1 кВ)",
+                        "img": "/image/kab4.jpg",
+                        "URL": "https://etipro.ru/catalogs/EG000001/EC003251"
+                    },
+                    {
+                        "name": "Кабель спиральный",
+                        "img": "/image/kab5.jpg",
+                        "URL": "https://etipro.ru/catalogs/EG000001/EC000247"
+                    }
+                ]
+            },
+            "kabel-provod-dlya-slabotochnix-system": {
+                "name": "Кабель, провод для слаботочных систем",
+                "img": "/image/kabel-provod-dlya-slabotochnix-system.jpg",
+                "items": [
+
+                ]
+            },
+            "shina": {
+                "name": "Шина",
+                "img": "/image/shina.jpg",
+                "items": [
+
+                ]
+            },
+            "shino-provod": {
+                "name": "Шинопровод",
+                "img": "/image/shino-provod.jpg",
+                "items": [
+
+                ]
+            }
+        }
+    }
+
+}
--- js/script.js
+++ js/script.js
@@ -1,15 +1,93 @@
 'use strict';
 
-let bottomUpArrow = document.querySelector('.bottomUpArrow');
-let dataArry = {};
-
+let bottomUpArrow = document.querySelector('.bottomUpArrow'),
+    main = document.querySelector('.main'),
+    itemCard = main.querySelectorAll('.item-card');
 
 const bottomToggle = () => {
     bottomUpArrow.classList.toggle('bottomActiv');
 };
 
+const searchElemP = () => {
+    let p = main.querySelector('.container>p');
+
+    if (p) {
+        let classP = p.className,
+            contentP = p.textContent;
+        return { classP, contentP };
+    }
+};
+
+// рендер входит до 3 вложения дальше ошибка
+const render = (naimId) => {
+    let dbLoop = '',
+        dbLoopTitle = '',
+        ahref = '#',
+        breadCrumsContent = '';
+
+    if (searchElemP()) {
+
+        let arr = searchElemP();
+        breadCrumsContent = arr.contentP;
+        dbLoop = dataDb[arr.classP]['items'][naimId]['items'];
+        dbLoopTitle = dataDb[arr.classP]['items'][naimId]['name'];
+    } else {
+        dbLoop = dataDb[naimId]['items'];
+        dbLoopTitle = dataDb[naimId]['name'];
+    }
+
+    main.textContent = '';
+    main = document.querySelector('.main');
+
+    let conteainer = document.createElement('div');
+    conteainer.classList.add('container');
+
+    let breadCrums = document.createElement('p');
+    breadCrums.classList.add(naimId);
+
+    if (breadCrumsContent) {
+        breadCrums.textContent = breadCrumsContent + ` > ` + dbLoopTitle;
+    } else {
+        breadCrums.textContent = dbLoopTitle;
+    }
+
+
+    let row = document.createElement('div');
+    row.classList.add('row', 'justify-content-start', 'items');
+
+    for (let key in dbLoop) {
+
+        if (dbLoop[key].URL) {
+            ahref = dbLoop[key].URL;
+        }
+
+        row.innerHTML += `
+            <div class="col-6 col-sm-4 col-md-3 col-xl-2 item-card">
+                <div class="item" id="${key}">
+                    <a href="${ahref}">
+                        <div class="item-img">
+                            <img src="${dbLoop[key].img}" loading="lazy" alt="${dbLoop[key].name}">
+                        </div>
+                        <hr>
+                        <div class="item-title">
+                            <h3>${dbLoop[key].name}</h3>
+                        </div>
+                    </a>
+                </div>            
+            </div>
+            `;
+    }
+
+    conteainer.append(breadCrums);
+    conteainer.append(row);
+    main.append(conteainer);
+    if (ahref === '#') addEventCart();
+};
+
+// СОБЫТИЯ 
+// скрол появление кнопки 
 document.addEventListener('scroll', () => {
-    // console.log(window.scrollY);
+
     if (window.scrollY >= 500) {
 
         if (!bottomUpArrow.classList.contains('bottomActiv')) {
@@ -22,6 +100,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+// плавное прокручивание кнопки до якоря
 bottomUpArrow.addEventListener('click', function (e) {
     e.preventDefault();
 
@@ -33,19 +112,18 @@ bottomUpArrow.addEventListener('click', function (e) {
     });
 });
 
-async function getJson() {
-    let response = await fetch('/db/db.json');
+// функция запуска слушателя
+function addEventCart() {
+    itemCard = main.querySelectorAll('.item-card');
 
-    let data = await response.json();
+    itemCard.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            let naimId = item.querySelector('.item').id;
+            render(naimId);
 
-    dataArry = data;
+        });
+    });
+};
 
-    return dataArry;
-}
-
-
-console.log(getJson());
-
-
-
-
+addEventCart();
